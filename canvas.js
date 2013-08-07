da7902671e6f9f9349ed8d04d567697ef0295034
Dan Tao <daniel.tao@gmail.com>
2013-08-07 16:43:36 -0700

    that's MUCH better

--- canvas.js
+++ canvas.js
@@ -13,28 +13,30 @@ window.addEventListener('load', function() {
   var canvasHeight = canvas.height;
   var context      = canvas.getContext('2d');
   var canvasData   = context.getImageData(0, 0, canvasWidth, canvasHeight);
+  var previousDot  = null;
   var drawing      = false;
+  var brushSize    = 1;
 
-  function drawDot(x, y, r, g, b, a) {
-    var index = (x + y * canvasWidth) * 4;
-
-    canvasData.data[index + 0] = r || 0;
-    canvasData.data[index + 1] = g || 0;
-    canvasData.data[index + 2] = b || 0;
-    canvasData.data[index + 3] = typeof a === 'number' ? a : 255;
+  function drawLine(start, end) {
+    context.beginPath();
+    context.moveTo.apply(context, start);
+    context.lineTo.apply(context, end);
+    context.lineCap = 'round';
+    context.lineWidth = brushSize;
+    context.stroke();
   }
 
-  function drawTo(x, y, r, g, b, a) {
-    drawDot(x, y, r, g, b, 255);
-  }
-
-  function updateCanvas() {
-    context.putImageData(canvasData, 0, 0);
+  function drawTo(x, y) {
+    if (previousDot) {
+      drawLine(previousDot, [x, y]);
+    }
+    previousDot = [x, y];
   }
 
   function toggleDrawing() {
     if (overlay.classList.contains('drawing')) {
       overlay.classList.remove('drawing');
+      previousDot = null;
       drawing = false;
     } else {
       overlay.classList.add('drawing');
@@ -42,9 +44,23 @@ window.addEventListener('load', function() {
     }
   }
 
+  function setBrushSize(size) {
+    var activeButton = document.querySelector('.brush-size.active');
+    activeButton.classList.remove('active');
+
+    var targetButton = document.querySelector('.brush-size:nth-child(' + size + ')');
+    targetButton.classList.add('active');
+
+    brushSize = (size * 2) - 1;
+  }
+
   overlay.addEventListener('click', function(e) {
     if (drawing) {
       drawTo(e.layerX, e.layerY, 0, 0, 0, 255);
+    }
+
+    if (e.target.nodeName === 'BUTTON') {
+      setBrushSize(Number(e.target.textContent));
     }
   });
 
@@ -55,6 +71,11 @@ window.addEventListener('load', function() {
   });
 
   document.addEventListener('keypress', function(e) {
+    if (e.charCode >= '1'.charCodeAt(0) && e.charCode <= '5'.charCodeAt(0)) {
+      setBrushSize(Number(String.fromCharCode(e.charCode)));
+      return;
+    }
+
     if (e.charCode === 'c'.charCodeAt(0)) {
       context.clearRect(0, 0, canvasWidth, canvasHeight);
       canvasData = context.getImageData(0, 0, canvasWidth, canvasHeight);
